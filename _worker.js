76460779cfe75f3f276bdb2cab9563f28a431061
jwyGithub <jwyGithub@users.noreply.github.com>
2024-09-30 00:58:19 +0000

    deploy: eba0e967f825e0d499389cd07233e4fcc2915f33

--- _worker.js
+++ _worker.js
@@ -1,51 +1,48 @@
-const w = {
+const y = {
   async fetch(t) {
-    return await f(t);
+    return await l(t);
   }
 };
-async function f(t) {
-  const s = new URL(t.url), o = `https://registry.npmjs.org${s.pathname}${s.search}`, r = t.method, u = new Headers(t.headers), i = s.pathname.startsWith("/-/user/org.couchdb.user:"), d = {
-    method: r,
+async function l(t) {
+  const s = new URL(t.url), o = `https://registry.npmjs.org${s.pathname}${s.search}`, n = t.method, u = new Headers(t.headers), i = s.pathname.startsWith("/-/user/org.couchdb.user:"), c = {
+    method: n,
     headers: u,
     body: t.body ? t.body : void 0,
     redirect: "follow"
   };
-  r === "PUT" && s.pathname.startsWith("/-/user/org.couchdb.user:") || r === "GET" && s.pathname.startsWith("/") || r === "PUT" && s.pathname.endsWith(".tgz") || r === "GET" && s.pathname.endsWith(".tgz");
-  const c = new Request(o, d);
+  n === "PUT" && s.pathname.startsWith("/-/user/org.couchdb.user:") || n === "GET" && s.pathname.startsWith("/") || n === "PUT" && s.pathname.endsWith(".tgz") || n === "GET" && s.pathname.endsWith(".tgz");
+  const d = new Request(o, c);
   let e;
   try {
-    e = await fetch(c);
+    e = await fetch(d);
   } catch (a) {
     return console.error("Failed to send request to npm registry", a), new Response("Internal Server Error", { status: 500 });
   }
   if (e.status === 201 && i && (e.headers.get("content-type") || "").includes("application/json")) {
-    let n;
+    let r;
     try {
-      n = await e.json();
-    } catch (y) {
-      return console.error("Failed to parse response JSON", y), new Response("Internal Server Error", { status: 500 });
+      r = await e.json();
+    } catch (p) {
+      return console.error("Failed to parse response JSON", p), new Response("Internal Server Error", { status: 500 });
     }
-    n.token;
-    const l = JSON.stringify(n);
-    return new Response(l, {
+    r.token;
+    const h = JSON.stringify(r);
+    return new Response(h, {
       status: e.status,
       statusText: e.statusText,
       headers: e.headers
     });
   }
-  if ([101, 204, 205, 304].includes(e.status))
-    return new Response(null, {
-      status: e.status,
-      statusText: e.statusText,
-      headers: e.headers
-    });
-  const { readable: h, writable: p } = new TransformStream();
-  return e.body && e.body.pipeTo(p), new Response(h, {
+  return [101, 204, 205, 304].includes(e.status) ? new Response(null, {
+    status: e.status,
+    statusText: e.statusText,
+    headers: e.headers
+  }) : new Response(e.body, {
     status: e.status,
     statusText: e.statusText,
     headers: e.headers
   });
 }
 export {
-  w as default
+  y as default
 };
