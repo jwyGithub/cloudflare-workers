71493bb6fdf0ba0de1ebc55df37778dc40a013f2
jiangwy <jiangwy@staryea.com>
2024-09-30 11:54:59 +0800

    cargo: update

--- packages/cargo-proxy/src/index.ts
+++ packages/cargo-proxy/src/index.ts
@@ -1,18 +1,21 @@
-async function handleRequest(req: Request): Promise<Response> {
-    const url = new URL(req.url);
-    if (url.pathname.startsWith('/crates.io-index/')) {
-        const newUrl = new URL('https://github.com/rust-lang/crates.io-index');
-        newUrl.pathname = url.pathname.slice('/crates.io-index/'.length);
-        newUrl.search = url.search;
-        return fetch(newUrl.toString(), req);
-    }
-    return fetch(req);
-}
+const CARGO_MIRROR = 'https://github.com/rust-lang/crates.io-index';
 
 export default {
     async fetch(request: Request): Promise<Response> {
-        return await handleRequest(request);
+        // 获取原始请求的URL
+        const url = new URL(request.url);
+
+        // 构造新的URL,将请求转发到Cargo镜像源
+        const newUrl = new URL(`${CARGO_MIRROR}${url.pathname}${url.search}`);
+
+        // 创建新的请求,保留原始请求的方法和头部信息
+        const newRequest = new Request(newUrl.toString(), {
+            method: request.method,
+            headers: request.headers,
+            body: request.body
+        });
+
+        // 发送请求到Cargo镜像源并返回响应
+        return fetch(newRequest);
     }
 };
-
-
