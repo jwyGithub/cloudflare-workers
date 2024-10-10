11643c06e0a785c3939ced3760ee1a0aa25b06de
jwyGithub <jwyGithub@users.noreply.github.com>
2024-10-10 07:35:12 +0000

    deploy: 9fa334c0c6dcd5e5315750bfb0ddcbbb3c00abd4

--- _worker.js
+++ _worker.js
@@ -1,45 +1,118 @@
-const y = {
-  async fetch(e) {
-    return await l(e);
+var h = (e) => {
+  throw TypeError(e);
+};
+var l = (e, t, s) => t.has(e) || h("Cannot " + s);
+var a = (e, t, s) => (l(e, t, "read from private field"), s ? s.call(e) : t.get(e)), p = (e, t, s) => t.has(e) ? h("Cannot add the same private member more than once") : t instanceof WeakSet ? t.add(e) : t.set(e, s), i = (e, t, s, o) => (l(e, t, "write to private field"), o ? o.call(e, s) : t.set(e, s), s);
+const I = "unauthorized", S = "internal server error", y = new Headers({
+  "Content-type": "application/json"
+}), H = new Headers({
+  "Content-type": "application/octet-stream"
+});
+new Headers({
+  "Content-type": "text/plain"
+});
+new Headers({
+  "Content-type": "text/html"
+});
+const W = (e, t = H) => new Response(e, {
+  status: 200,
+  headers: t
+}), b = (e = I, t = 401, s = y) => new Response(
+  JSON.stringify({
+    status: t,
+    message: e
+  }),
+  {
+    status: t,
+    statusText: e,
+    headers: s
+  }
+), d = (e = S, t = 500, s = y) => new Response(
+  JSON.stringify({
+    status: t,
+    message: e
+  }),
+  {
+    status: t,
+    statusText: e,
+    headers: s
+  }
+), j = /^(?:(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(?:\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])(?::(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/;
+function v(e) {
+  return e ? j.test(e) : !1;
+}
+function A(e, t) {
+  if (!e || !t || !v(e)) return !1;
+  for (let s = 0; s < t.length; s++)
+    if (new RegExp(t[s].replace(/\./g, "\\.").replace(/\*/g, "\\d+")).test(e))
+      return !0;
+  return !1;
+}
+var n;
+class N {
+  constructor() {
+    p(this, n, []);
+    i(this, n, []);
+  }
+  setEnv(t) {
+    if (a(this, n).length || !Reflect.has(t, "IP_WHITELIST")) return;
+    const s = Reflect.get(t, "IP_WHITELIST") ?? "*";
+    s === "*" ? i(this, n, "*") : i(this, n, s.split(",").map((o) => o.trim()));
+  }
+  checkIpIsWhitelisted(t) {
+    return (typeof a(this, n) == "string" && a(this, n)) === "*" || Array.isArray(a(this, n)) && a(this, n).length === 0 ? !0 : Array.isArray(a(this, n)) && a(this, n).length > 0 ? A(t, a(this, n)) : !1;
+  }
+}
+n = new WeakMap();
+const f = new N(), J = {
+  async fetch(e, t) {
+    try {
+      const { pathname: s } = new URL(e.url);
+      f.setEnv(t);
+      const o = e.headers.get("x-real-ip");
+      return f.checkIpIsWhitelisted(o) ? s === "/favicon.ico" ? W("", e.headers) : await _(e) : b();
+    } catch (s) {
+      return d(s.message);
+    }
   }
 };
-async function l(e) {
-  const s = new URL(e.url), a = `https://registry.npmjs.org${s.pathname}${s.search}`, o = e.method, u = new Headers(e.headers), c = s.pathname.startsWith("/-/user/org.couchdb.user:"), d = {
+async function _(e) {
+  const t = new URL(e.url), s = `https://registry.npmjs.org${t.pathname}${t.search}`, o = e.method, g = new Headers(e.headers), w = t.pathname.startsWith("/-/user/org.couchdb.user:"), x = {
     method: o,
-    headers: u,
+    headers: g,
     body: e.body ? e.body : void 0,
     redirect: "follow"
-  }, i = new Request(a, d);
-  let t;
+  }, m = new Request(s, x);
+  let r;
   try {
-    t = await fetch(i);
-  } catch (r) {
-    return console.error("Failed to send request to npm registry", r), new Response("Internal Server Error", { status: 500 });
+    r = await fetch(m);
+  } catch (c) {
+    return console.error("Failed to send request to npm registry", c), d();
   }
-  if (t.status === 201 && c && (t.headers.get("content-type") || "").includes("application/json")) {
-    let n;
+  if (r.status === 201 && w && (r.headers.get("content-type") || "").includes("application/json")) {
+    let u;
     try {
-      n = await t.json();
-    } catch (p) {
-      return console.error("Failed to parse response JSON", p), new Response("Internal Server Error", { status: 500 });
+      u = await r.json();
+    } catch (T) {
+      return console.error("Failed to parse response JSON", T), d();
     }
-    const h = JSON.stringify(n);
-    return new Response(h, {
-      status: t.status,
-      statusText: t.statusText,
-      headers: t.headers
+    const R = JSON.stringify(u);
+    return new Response(R, {
+      status: r.status,
+      statusText: r.statusText,
+      headers: r.headers
     });
   }
-  return [101, 204, 205, 304].includes(t.status) ? new Response(null, {
-    status: t.status,
-    statusText: t.statusText,
-    headers: t.headers
-  }) : new Response(t.body, {
-    status: t.status,
-    statusText: t.statusText,
-    headers: t.headers
+  return [101, 204, 205, 304].includes(r.status) ? new Response(null, {
+    status: r.status,
+    statusText: r.statusText,
+    headers: r.headers
+  }) : new Response(r.body, {
+    status: r.status,
+    statusText: r.statusText,
+    headers: r.headers
   });
 }
 export {
-  y as default
+  J as default
 };
