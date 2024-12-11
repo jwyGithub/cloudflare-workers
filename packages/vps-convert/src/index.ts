aec2383862f9448226268d868dc250c704a54d88
jiangwy <jiangwy@staryea.com>
2024-12-12 01:13:08 +0800

    convert: 增加base64类型的订阅链接

--- packages/vps-convert/src/index.ts
+++ packages/vps-convert/src/index.ts
@@ -1,11 +1,10 @@
 import { fetchWithRetry } from '@jiangweiye/worker-fetch';
-import { toServerError, toStream } from '@jiangweiye/worker-service';
-import { dump, load } from 'js-yaml';
+import { toClientError, toServerError, toStream } from '@jiangweiye/worker-service';
+import { dump } from 'js-yaml';
 import { getConfuseUrl } from './confuse';
 import { getOriginConfig } from './confuse/restore';
 import { SERVICE_GET_SUB } from './constants';
 import { DEFAULT_CONFIG, showPage } from './page';
-import type { Clash } from './types/Clash';
 
 export default {
     async fetch(request: Request, env: Env): Promise<Response> {
@@ -13,14 +12,15 @@ export default {
             const { pathname, origin } = new URL(request.url);
             if (pathname === '/sub') {
                 const { confuseUrl, vpsMap } = await getConfuseUrl(request.url, env.BACKEND ?? DEFAULT_CONFIG.BACKEND);
-                console.log(`confuseUrl: ${confuseUrl}`);
                 const response = await fetchWithRetry(confuseUrl, { retries: 3 });
                 if (!response.ok) {
                     throw new Error(response.statusText);
                 }
-                console.log(`confuseConfig: ${response.status} ${response.statusText}`);
                 const confuseConfig = await response.data.text();
-                console.log(`confuseConfig: ${(load(confuseConfig) as Clash).proxies}`);
+                if (!confuseConfig) {
+                    return toClientError('confuseConfig is empty');
+                }
+
                 const originConfig = getOriginConfig(confuseConfig, vpsMap);
                 return toStream(
                     dump(originConfig, { indent: 2, lineWidth: 200 }),
@@ -33,6 +33,9 @@ export default {
 
             if (pathname === `/${SERVICE_GET_SUB}`) {
                 const cacheResponse = await caches.default.match(`${origin}/${SERVICE_GET_SUB}`);
+                if (!cacheResponse) {
+                    console.log('cache not found');
+                }
                 return cacheResponse || toServerError('Cache not found');
             }
 
