62a79fe9b19711667557395ca463b480c8ca84dc
jiangwy <jiangwy@staryea.com>
2025-01-02 16:31:10 +0800

    short: add option request

--- packages/short-url/src/index.ts
+++ packages/short-url/src/index.ts
@@ -10,6 +10,22 @@ export default {
         const service = new UrlService(env.DB);
         const controller = new UrlController(service);
 
+        // 添加cors请求头
+        request.headers.set('Access-Control-Allow-Origin', '*');
+        request.headers.set('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+        request.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+        if (request.method === 'OPTIONS') {
+            return new Response(null, {
+                status: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, DELETE',
+                    'Access-Control-Allow-Headers': 'Content-Type'
+                }
+            });
+        }
+
         router
             .get('/', () => handlePageRequest())
             .post('/api/add', req => controller.add(req))
